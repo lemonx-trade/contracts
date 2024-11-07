3e178a4515ed2d5e16bd2a73c656a64056ea3df1
fedluca <168505096+fedluca@users.noreply.github.com>
2024-11-07 12:45:49 +0530

    chore: simplify listing process in `ListNewPerp.s.sol` (simulation verified)
    This update improves the ease of listing by refining `ListNewPerp.s.sol`. Simulation runs successfully with the following command:
    ```
    forge script script/ListNewPerp.s.sol --rpc-url $CORE_RPC_URL -vvvv --via-ir --legacy
    ```

--- script/ListNewPerp.s.sol
+++ script/ListNewPerp.s.sol
@@ -8,41 +8,41 @@ import "../src/contracts/core/PriceFeed.sol";
 import "../src/contracts/core/Utils.sol";
 
 contract AddNewIndexToken is Script {
-    function run() external {
-        // Retrieve the deployer's private key from environment variables
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_ADMIN");
-        vm.startBroadcast(deployerPrivateKey);
-        vm.txGasPrice(50); // in gwei but it doesn't solve the problem "Failed to get EIP 1559 fees" estimate
+    function addNewIndexToken(
+        string memory perpName,
+        address utilsAddress,
+        address vaultAddress,
+        address priceFeedAddress,
+        uint256 maxGlobalLongSizeNewPerp,
+        uint256 maxGlobalShortSizeNewPerp,
+        uint256 oiImbalanceThresholdNewPerp,
+        uint256 globalLongSizesLimitBpsForNewPerp,
+        uint256 globalShortSizesLimitBpsForNewPerp,
+        uint256 slippage,
+        uint256 maintanenceMarginForNewPerp,
+        uint256 borrowingExponent,
+        uint256 borrowingInterval,
+        uint256 borrowingRateFactor,
+        uint256 fundingExponent,
+        uint256 fundingInterval,
+        uint256 fundingRateFactor
+    ) internal {
+        // Construct environment variable names
+        string memory envPerpAddress = perpName;
+        string memory envPerpDecimals = string(abi.encodePacked(perpName, "_DECIMAL"));
+        string memory envPerpPriceId = string(abi.encodePacked(perpName, "_PYTH_FEED_MAINNET"));
+        string memory envPerpMaxLeverage = string(abi.encodePacked(perpName, "_MAX_LEVERAGE"));
 
-        // Retrieve contract addresses from environment variables
-        address utilsAddress = vm.envAddress("UTILS");
-        address vaultAddress = vm.envAddress("VAULT");
-        address priceFeedAddress = vm.envAddress("PRICEFEED");
+        // Validate that all environment variables are set
+        address newPerp = vm.envAddress(envPerpAddress);
+        uint256 newPerpDecimals = vm.envUint(envPerpDecimals);
+        bytes32 newPerpPriceId = vm.envBytes32(envPerpPriceId);
+        uint256 maxLeverage = vm.envUint(envPerpMaxLeverage);
 
-        // Define the new Perp's parameters
-        address newPerp = vm.envAddress("TON");
-        uint256 newPerpDecimals = vm.envUint("TON_DECIMAL");
-        bytes32 newPerpPriceId = vm.envBytes32("TON_PYTH_FEED_MAINNET");
-        uint256 maxLeverage = vm.envUint("MAX_LEVERAGE");
-        // Instantiate the Vault and PriceFeed contracts
+        // Instantiate the Vault, PriceFeed, and Utils contracts
         Vault vault = Vault(vaultAddress);
         PriceFeed priceFeed = PriceFeed(priceFeedAddress);
-        uint256 maxGlobalLongSizeNewPerp = 4_000;
-        uint256 maxGlobalShortSizeNewPerp = 4_000;
-        uint256 oiImbalanceThresholdNewPerp = 10_000;
-        uint256 globalLongSizesLimitBpsForNewPerp = 10_000;
-        uint256 globalShortSizesLimitBpsForNewPerp = 10_000;
-
-        uint256 borrowingExponent = 1;
-        uint256 borrowingInterval = 60;
-        uint256 borrowingRateFactor = 0;
-        uint256 fundingExponent = 1;
-        uint256 fundingInterval = 60;
-        uint256 fundingRateFactor = 24353121;
-
-        uint256 slippage = 500;
-
-        uint256 maintanenceMarginForNewPerp = 100;
+        Utils utils = Utils(utilsAddress);
 
         // 1. Configure the Vault with the new Perp
         vault.setTokenConfig(
@@ -52,62 +52,151 @@ contract AddNewIndexToken is Script {
             false, // isStable
             false, // canBeCollateralToken
             true, // canBeIndexToken
-            maxLeverage // maxLeverage (adjust as needed)
+            maxLeverage // maxLeverage
         );
-        console.log("Vault: Token configuration set for new Perp.", newPerp);
+        console.log("Vault: Token configuration set for new Perp %s. Address: %s", perpName, newPerp);
 
         // 2. Set maximum global long and short sizes for the new Perp
         vault.setMaxGlobalLongSize(newPerp, maxGlobalLongSizeNewPerp);
         vault.setMaxGlobalShortSize(newPerp, maxGlobalShortSizeNewPerp);
-        console.log("Vault: Set max global long size for new Perp. newPerp => ", newPerp);
-        console.log("maxGlobalLongSizeNewPerp => ", maxGlobalLongSizeNewPerp);
-
-        console.log("Vault: Set max global short size for new Perp. newPerp => ", newPerp);
-        console.log("maxGlobalShortSizeNewPerp => ", maxGlobalShortSizeNewPerp);
+        console.log("Vault: Set max global long size for %s: %s", perpName, maxGlobalLongSizeNewPerp);
+        console.log("Vault: Set max global short size for %s: %s", perpName, maxGlobalShortSizeNewPerp);
 
         vault.setGlobalLongSizesLimitBps(newPerp, globalLongSizesLimitBpsForNewPerp);
         vault.setGlobalShortSizesLimitBps(newPerp, globalShortSizesLimitBpsForNewPerp);
 
         // 3. Define the OI imbalance threshold for the new Perp
         vault.setOiImbalanceThreshold(newPerp, oiImbalanceThresholdNewPerp);
-        console.log("Vault: Set OI imbalance threshold for new Perp. newPerp => ", newPerp);
-        console.log("oiImbalanceThresholdNewPerp => ", oiImbalanceThresholdNewPerp);
+        console.log("Vault: Set OI imbalance threshold for %s: %s", perpName, oiImbalanceThresholdNewPerp);
 
         // 4. Configure borrowing rates for the new Perp
         vault.setBorrowingRate(newPerp, borrowingInterval, borrowingRateFactor, borrowingExponent);
-        console.log("Vault: Borrowing rate configured for new Perp. newPerp => ", newPerp);
-        console.log("borrowingInterval => ", borrowingInterval);
-        console.log("borrowingRateFactor => ", borrowingRateFactor);
-        console.log("borrowingExponent => ", borrowingExponent);
+        console.log("Vault: Borrowing rate configured for %s.", perpName);
+        console.log("Borrowing Interval: %s", borrowingInterval);
+        console.log("Borrowing Rate Factor: %s", borrowingRateFactor);
+        console.log("Borrowing Exponent: %s", borrowingExponent);
 
         // 5. Configure funding rates for the new Perp
         vault.setFundingRate(newPerp, fundingInterval, fundingRateFactor, fundingExponent);
-        console.log("Vault: Funding rate configured for new Perp. newPerp => ", newPerp);
-        console.log("fundingInterval => ", fundingInterval);
-        console.log("fundingRateFactor => ", fundingRateFactor);
-        console.log("fundingExponent => ", fundingExponent);
+        console.log("Vault: Funding rate configured for %s.", perpName);
+        console.log("Funding Interval: %s", fundingInterval);
+        console.log("Funding Rate Factor: %s", fundingRateFactor);
+        console.log("Funding Exponent: %s", fundingExponent);
 
         // 6. Update the PriceFeed with the new Perp's price ID mapping
         priceFeed.updateTokenIdMapping(newPerp, newPerpPriceId);
-        console.log("PriceFeed: Token ID mapping updated for new Perp. newPerp => ", newPerp);
-        // console.log("newPerpPriceId => ", newPerpPriceId);
+        console.log("PriceFeed: Token ID mapping updated for %s.", perpName);
 
         // 7. Set slippage parameters for the new Perp in PriceFeed
-        priceFeed.setSlippage(newPerp, slippage); // Adjust slippage as needed
-        console.log("PriceFeed: Slippage set for new Perp. newPerp => ", newPerp);
-        console.log("slippage => ", slippage);
+        priceFeed.setSlippage(newPerp, slippage);
+        console.log("PriceFeed: Slippage set for %s: %s", perpName, slippage);
 
-        // 8. Set maintanence margin for the new Perp in the utils contract
-        Utils utils = Utils(utilsAddress);
+        // 8. Set maintenance margin for the new Perp in the utils contract
         utils.setMaintanenceMargin(newPerp, maintanenceMarginForNewPerp);
-        console.log("Utils: Maintanence margin set for new Perp. newPerp => ", newPerp);
-        console.log("maintanenceMarginForNewPerp => ", maintanenceMarginForNewPerp);
+        console.log("Utils: Maintenance margin set for %s: %s", perpName, maintanenceMarginForNewPerp);
 
         // 9. Set premium position fee for the new Perp in the utils contract
-        utils.setTokenPremiumPositionFee(newPerp, 10);
-        console.log("Utils: Premium position fee set for new Perp. newPerp => ", newPerp);
-        console.log("premiumPositionFee => ", 10);
+        int256 premiumPositionFee = 10; // Default value
+        utils.setTokenPremiumPositionFee(newPerp, premiumPositionFee);
+        // console.log("Utils: Premium position fee set for %s: %s", perpName, premiumPositionFee);
+    }
+
+    function run() external {
+        // Retrieve the deployer's private key from environment variables
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_ADMIN");
+        vm.startBroadcast(deployerPrivateKey);
+        vm.txGasPrice(50); // in gwei
+
+        // Retrieve contract addresses from environment variables
+        address utilsAddress = vm.envAddress("UTILS");
+        address vaultAddress = vm.envAddress("VAULT");
+        address priceFeedAddress = vm.envAddress("PRICEFEED");
+
+        // Example: Add DOGE Perp
+        addDOGE(utilsAddress, vaultAddress, priceFeedAddress);
 
         vm.stopBroadcast();
+    }
+
+    function addTON(address utilsAddress, address vaultAddress, address priceFeedAddress) internal {
+        // Parameters specific to TON
+        string memory perpName = "TON";
+        uint256 maxGlobalLongSizeNewPerp = 4_000;
+        uint256 maxGlobalShortSizeNewPerp = 4_000;
+        uint256 oiImbalanceThresholdNewPerp = 10_000;
+        uint256 globalLongSizesLimitBpsForNewPerp = 10_000;
+        uint256 globalShortSizesLimitBpsForNewPerp = 10_000;
+        uint256 slippage = 500;
+        uint256 maintanenceMarginForNewPerp = 300;
+
+        // Optional parameters (can be overridden)
+        uint256 borrowingExponent = 1;
+        uint256 borrowingInterval = 60;
+        uint256 borrowingRateFactor = 0;
+        uint256 fundingExponent = 1;
+        uint256 fundingInterval = 60;
+        uint256 fundingRateFactor = 24353121;
+
+        // Call the main function with these parameters
+        addNewIndexToken(
+            perpName,
+            utilsAddress,
+            vaultAddress,
+            priceFeedAddress,
+            maxGlobalLongSizeNewPerp,
+            maxGlobalShortSizeNewPerp,
+            oiImbalanceThresholdNewPerp,
+            globalLongSizesLimitBpsForNewPerp,
+            globalShortSizesLimitBpsForNewPerp,
+            slippage,
+            maintanenceMarginForNewPerp,
+            borrowingExponent,
+            borrowingInterval,
+            borrowingRateFactor,
+            fundingExponent,
+            fundingInterval,
+            fundingRateFactor
+        );
+    }
+
+    function addDOGE(address utilsAddress, address vaultAddress, address priceFeedAddress) internal {
+        // Parameters specific to TON
+        string memory perpName = "DOGE";
+        uint256 maxGlobalLongSizeDOGE = 2_000;
+        uint256 maxGlobalShortSizeDOGE = 2_000;
+        uint256 oiImbalanceThresholdDOGE = 5_000;
+        uint256 globalLongSizesLimitBpsForDOGE = 5_000;
+        uint256 globalShortSizesLimitBpsForDOGE = 5_000;
+        uint256 slippage = 500;
+        uint256 maintanenceMarginForDOGE = 700;
+
+        // Optional parameters (can be overridden)
+        uint256 borrowingExponent = 1;
+        uint256 borrowingInterval = 60;
+        uint256 borrowingRateFactor = 0;
+        uint256 fundingExponent = 1;
+        uint256 fundingInterval = 60;
+        uint256 fundingRateFactor = 24353121;
+
+        // Call the main function with these parameters
+        addNewIndexToken(
+            perpName,
+            utilsAddress,
+            vaultAddress,
+            priceFeedAddress,
+            maxGlobalLongSizeDOGE,
+            maxGlobalShortSizeDOGE,
+            oiImbalanceThresholdDOGE,
+            globalLongSizesLimitBpsForDOGE,
+            globalShortSizesLimitBpsForDOGE,
+            slippage,
+            maintanenceMarginForDOGE,
+            borrowingExponent,
+            borrowingInterval,
+            borrowingRateFactor,
+            fundingExponent,
+            fundingInterval,
+            fundingRateFactor
+        );
     }
 }
